58729daa4daf9cc01062797446b7d756685c85d7
Mir <mirbilal@Muneebs-MacBook-Pro.local>
2024-06-22 23:54:37 +0500

    ins2

--- apps/email_manager/admin.py
+++ apps/email_manager/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.email_manager.models.email import EmailAttachment, EmailData
+
+@admin.register(EmailData)
+class EmailDataAdmin(admin.ModelAdmin):
+    list_display = ['id', 'from_email', 'subject', 'to_emails']
+    search_fields = ['id', 'from_email', 'subject', 'to_emails']
+
+@admin.register(EmailAttachment)
+class EmailAttachmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'name',]
+    search_fields = ['id', 'name', "email__subject"]
---
+++ apps/email_manager/models/email.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from apps.client_manager.models.client import Client
+from apps.risk_manager.models.risk import Risk
+
+class EmailData(models.Model):
+    from_email = models.CharField(max_length=255)
+    to_emails = ArrayField(models.CharField(max_length=255), blank=True, default=list)
+    subject = models.TextField(blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        # ordering = ['-date']
+        verbose_name_plural = "       Emails Data"
+        db_table = 'emails'
+
+
+class EmailAttachment(models.Model):
+    email = models.ForeignKey(EmailData, related_name='attachments', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='email_attachments/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        # ordering = ['-date']
+        verbose_name_plural = "       Email Attachments"
+        db_table = 'email_attachments'
