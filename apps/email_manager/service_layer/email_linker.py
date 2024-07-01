95fc993f74f308588e4945c539eb84330f7d0563
Mir <mirbilal@Muneebs-MacBook-Pro.local>
2024-07-02 00:39:39 +0500

    ins22

---
+++ apps/email_manager/service_layer/email_linker.py
@@ -0,0 +1,258 @@
+from datetime import timedelta
+import re
+from apps.email_manager.models import EmailData
+import re
+
+from apps.email_manager.models.email import EmailAttachment
+# what is the longest length of convo
+# how to extract one conversations from the data
+
+class EmailNode:
+    def __init__(self, previous_node, email_data, next_node):
+        self.previous_node: EmailNode = previous_node
+        self.email_data: EmailData = email_data
+        self.next_node: EmailNode = next_node
+
+class EmailConvo:
+    def __init__(self, first_email):
+        email_node = EmailNode(
+            previous_node=None,
+            email_data=first_email,
+            next_node=None
+        )
+        self.head_email_node = email_node
+        self.tail_email_node = email_node
+    
+    def append_email_node_to_convo(self, new_email):
+        next_node = EmailNode(
+            previous_node=self.tail_email_node,
+            email_data=new_email,
+            next_node=None
+        )
+        self.tail_email_node.next_node = next_node
+        self.tail_email_node = next_node
+
+    def prepend_email_node_to_convo(self, new_email):
+        first_node = EmailNode(
+            previous_node=None,
+            email_data=new_email,
+            next_node=self.head_email_node
+        )
+        self.head_email_node.previous_node = first_node
+        self.head_email_node = first_node
+
+    def remove_last_email(self):
+        new_last_email = self.tail_email_node.previous_node
+        if new_last_email is not None:
+            new_last_email.next_node = None
+            self.tail_email_node = new_last_email
+
+    def remove_first_email(self):
+        new_first_email = self.head_email_node.next_node
+        if new_first_email is not None:
+            new_first_email.previous_node = None
+            self.head_email_node = new_first_email
+    
+    def remove_email_from_convo(self, index = None, e_id = None):
+        if (index is not None and id is not None) or (index is None and id is None):
+            return -1
+        iter = 0
+        def _remove_email_from_convo(current_node: EmailNode):
+            nonlocal iter
+            if (e_id is not None and current_node.email_data.id == e_id) or (index is not None and iter==index):
+                previous_node: EmailNode = current_node.previous_node
+                next_node: EmailNode = current_node.next_node
+                if previous_node is not None and next_node is not None:
+                    previous_node.next_node = next_node
+                    next_node.previous_node = previous_node
+                elif previous_node is not None and next_node is None:
+                    previous_node.next_node = None
+                elif previous_node is None and next_node is not None:
+                    next_node.previous_node = None
+                    self.head_email_node = next_node
+                elif previous_node is None and next_node is None:
+                    return 0
+                return 1
+            else:
+                next_node = current_node.next_node
+                if next_node is not None:
+                    iter = iter + 1
+                    return _remove_email_from_convo(next_node)
+                else:
+                    return -1
+        return _remove_email_from_convo(self.head_email_node)
+    
+    def insert_email_into_convo(self, new_email: EmailData, index = None, e_id = None):
+        if (index is not None and id is not None) or (index is None and id is None):
+            return -1
+        iter = 0
+        def _insert_email_into_convo(current_node: EmailNode):
+            nonlocal iter
+            if (e_id is not None and current_node.email_data.id == e_id) or (index is not None and iter==index):
+                new_email_node = EmailNode(
+                    previous_node=None,
+                    email_data=new_email,
+                    next_node=None
+                )
+                previous_node: EmailNode = current_node.previous_node
+                next_node: EmailNode = current_node.next_node
+                if previous_node is not None:
+                    previous_node.next_node = new_email_node
+                    new_email_node.previous_node = previous_node
+
+                    current_node.previous_node = new_email_node
+                    new_email_node.next_node = current_node
+
+                else:
+                    current_node.previous_node = new_email_node
+                    new_email_node.next_node = current_node
+                    self.head_email_node = new_email_node
+                return 1
+            else:
+                next_node = current_node.next_node
+                if next_node is not None:
+                    iter = iter + 1
+                    return _insert_email_into_convo(next_node)
+                else:
+                    return -1
+        return _insert_email_into_convo(self.head_email_node)
+    
+    def retrieve_email_from_convo(self, index = None, e_id = None, text = None):
+        if (index is None and id is None and text is None):
+            return -1
+        iter = 0
+        def _retrieve_email_from_convo(current_node: EmailNode):
+            nonlocal iter
+            if (
+                (e_id is not None and current_node.email_data.id == e_id) 
+                or (index is not None and iter==index) 
+                or (current_node.email_data.body is not None and text in current_node.email_data.body)
+            ):
+                return current_node.email_data
+            else:
+                next_node = current_node.next_node
+                if next_node is not None:
+                    iter = iter + 1
+                    return _retrieve_email_from_convo(next_node)
+                else:
+                    return None
+        return _retrieve_email_from_convo(self.head_email_node)
+    
+    def get_clean_emails(self, email_strings):
+
+        # Regular expression to extract email addresses
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+        # Initialize an empty list to store the extracted email addresses
+        all_emails = []
+
+        # Iterate over each string in the list
+        for email_string in email_strings:
+            # Find all email addresses in the current string
+            emails = re.findall(email_pattern, email_string)
+            # Extend the all_emails list with the found email addresses
+            all_emails.extend([email.lower() for email in emails])
+
+        # Print the list of all extracted email addresses
+        return all_emails
+    
+    def get_full_convo(self):
+        first_email = self.head_email_node
+        full_convo = []
+        attachments_dict = {}
+        def _get_full_convo(current_email: EmailNode):
+            email_data = current_email.email_data
+            full_convo.append({
+                "from": self.get_clean_emails([email_data.from_email])[0],
+                "to": self.get_clean_emails(email_data.to_emails),
+                "date": email_data.date.strftime('%B %d, %Y, %-I:%M %p'),
+                "subject": email_data.subject,
+                "body": email_data.body
+            })
+            next_email = current_email.next_node
+            attachments: list[EmailAttachment] = email_data.attachments.all()
+            for an_attchmnt in attachments:
+                if an_attchmnt.name is not None:
+                    attachments_dict[an_attchmnt.id] = an_attchmnt
+
+
+            if next_email is not None:
+                return _get_full_convo(next_email)
+            else:
+                return None
+        _get_full_convo(first_email)
+        return (full_convo, list(attachments_dict.values()))
+    
+# if it is of the form re, but before the re, there is one without the re, then split
+# create one key for each to email
+# check from each
+
+
+class ConversationReader:
+    def __init__(self):
+        self.conversations = {}
+        self.latest_subjects = {}
+        self.re_list = [
+            "RE: ",
+            "Re: ",
+            "re: "
+        ]
+        self.fwd_list = [
+            "FW: ",
+            "fw: ",
+            "Fw: ",
+            "FWD: ",
+            "Fwd: ",
+            "fwd: "
+        ]
+
+    def extract_conversation(self):
+        emails = EmailData.objects.order_by('date').prefetch_related("attachments").all()[0:1500]
+        for an_email in emails:
+            email_date = an_email.date
+            if an_email.subject and email_date:
+                is_reply = False
+                is_fwdd = False
+                original_subject = an_email.subject.strip()
+                
+                if any(substring in original_subject for substring in self.re_list):
+                    is_reply = True
+                if any(substring in original_subject for substring in self.fwd_list):
+                    is_fwdd = True
+                rmvd_subject = original_subject
+                for re_substring in self.re_list:
+                    rmvd_subject = rmvd_subject.replace(re_substring, "")
+                for fwd_substring in self.fwd_list:
+                    rmvd_subject = rmvd_subject.replace(fwd_substring, "")
+                rmvd_subject = rmvd_subject.replace("\n", "").replace("\r", "")
+                if rmvd_subject in self.latest_subjects:
+                    existing_latest_date = self.latest_subjects[rmvd_subject]
+                    date_difference = email_date - existing_latest_date
+                    if not is_reply and not is_fwdd and date_difference > timedelta(days=30):
+                        latest_date = email_date
+                        self.latest_subjects[rmvd_subject] = latest_date
+                    else:
+                        latest_date = self.latest_subjects[rmvd_subject]
+                else:
+                    latest_date = email_date
+                    self.latest_subjects[rmvd_subject] = latest_date
+                # if 'RENEWAL | M/S HYGIENE CONTAINERS (PRIVATE) LIMITED | FIRE POLICIES' in rmvd_subject:
+                #     print(get_clean_emails([an_email.from_email]))
+                #     print(get_clean_emails(an_email.to_emails))
+                #     print(original_subject)
+                #     print(rmvd_subject)
+                #     print("subject in dict", rmvd_subject in self.latest_subjects)
+                #     print("is_new ",not is_reply and not is_fwdd and date_difference > timedelta(days=30))
+                #     print(email_date.strftime('%B %d, %Y, %-I:%M %p'))
+                #     print("")
+                formatted_date = latest_date.strftime('%B %d, %Y, %-I:%M %p')
+                key_str = f"{formatted_date}@{rmvd_subject}"
+                if key_str in self.conversations:
+                    convo_emails: EmailConvo = self.conversations[key_str]
+                    convo_emails.append_email_node_to_convo(an_email)
+                else:
+                    self.conversations[key_str] = EmailConvo(first_email=an_email)
+
+            
+
+        
---
+++ apps/email_manager/service_layer/insurance_erp_llm.py
@@ -0,0 +1,150 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage
+)
+import json
+import torch
+
+
+class InsuranceERPLLM:
+    def __init__(self):
+        torch.cuda.empty_cache()
+
+        # Load embeddings model
+        model_name = 'sentence-transformers/paraphrase-mpnet-base-v2'
+
+        # Create FAISS index from documents
+        self.embeddings_model = HuggingFaceEmbeddings(model_name=model_name)
+        self.documents_vector_store = None
+
+        self.num_of_choices = 3
+
+        self.queries = {
+            "net_premium": "What is the net premium amount?",
+            "issue_date": "What is the starting date of the coverage period?",
+            "covernote_number": "What is the cover note number which starts with 'COV'? Rule: It must start with 'COV'",
+            "policy_number": "What is the policy number which starts with 'PL'? Rule: It must start with 'PL'"
+        }
+
+        systm_msgs = [
+            "You are a helpful assistant that will help the user to figure out the answer to the related query given the content-data.",
+            f"The content data will contain {self.num_of_choices} choices for the most relevant data, you have to use a combination of the choices to answer the query.",
+            "Do not perform any mathematical operations on your own, all the data will be contained within the choices.",
+            "The answer will be singular. use an '=' sign before the answer.",
+            "If you cannot find the answer easily, say the following: '404 Not Found'.",
+            "There should be only one '=' sign in the answer if it is found.",
+            "for any date that is found, convert it into the following format: YYYY-MM-DD.",
+            "Do not perform any mathematical operations on your own, all the data will be contained within the choices.",
+            "If the answer or amount or date is found, use an '=' sign before the answer or amount or date."
+        ]
+        self.systm_msg = "\n".join(systm_msgs)
+        self.doc_chunk_size = 1000
+        self.doc_chunk_overlap=200
+        self.convo_chunk_size = 500
+        self.convo_chunk_overlap=100
+        self.document_query_results = {}
+        self.convo_query_results = {}
+
+    def store_documents_in_vector_store(self, file_paths):
+        all_documents = []
+        
+        for path in file_paths:
+            # Load PDF and split into documents
+            loader = PyPDFLoader(path)
+            raw_documents = loader.load()
+
+            # Split documents into smaller chunks
+            text_splitter = CharacterTextSplitter(chunk_size=self.doc_chunk_size, chunk_overlap=self.doc_chunk_overlap, separator="\n")
+            documents = text_splitter.split_documents(raw_documents)
+            all_documents.extend(documents)
+
+            # Debug: Print the number of documents and their lengths
+            # print(f"Number of documents from {path}: {len(documents)}")
+            # for i, doc in enumerate(documents):
+            #     print(f"Document {i} length: {len(doc.page_content)}")
+            #     print(f"Document {i} content: {doc.page_content[:200]}...")
+
+        # Create FAISS index from all documents
+        self.documents_vector_store = FAISS.from_documents(all_documents, self.embeddings_model)
+
+    def store_convo_in_vector_store(self, convo):
+        all_texts = []
+        
+        # Load PDF and split into documents
+
+        # Split documents into smaller chunks
+        text_splitter = CharacterTextSplitter(chunk_size=self.convo_chunk_size, chunk_overlap=self.convo_chunk_overlap, separator=" ")
+        texts_chunks = text_splitter.split_text(convo)
+        all_texts.extend(texts_chunks)
+
+        # Debug: Print the number of documents and their lengths
+        # print(f"Number of documents from {path}: {len(documents)}")
+        # for i, doc in enumerate(documents):
+        #     print(f"Document {i} length: {len(doc.page_content)}")
+        #     print(f"Document {i} content: {doc.page_content[:200]}...")
+
+        self.convo_vector_store = FAISS.from_texts(all_texts, self.embeddings_model)
+
+
+    def get_vectors(self, vector_store):
+        vectors_dict = {}
+        if vector_store and vector_store.index:
+            vectors = vector_store.index.reconstruct_n(0, vector_store.index.ntotal)
+            for i, vector in enumerate(vectors):
+                vectors_dict[i] = vector
+        return vectors_dict
+
+    def search_documents_vector_store(self):
+
+        # Retrieve and print the top three relevant chunks for each query
+        results = {}
+        for key, query in self.queries.items():
+            retrieved_docs = self.documents_vector_store.similarity_search(query, k=self.num_of_choices)
+            results[key] = [doc.page_content for doc in retrieved_docs] if retrieved_docs else ["Not found"]
+
+        # Print the top three relevant chunks for each query
+        self.document_query_results = {}
+        for key, contents in results.items():
+            self.document_query_results[key] = {}
+            for i, content in enumerate(contents):
+                self.document_query_results[key][f"Choice {i+1}"] = content
+
+        print(json.dumps(self.document_query_results, indent=4))
+        print("Number of tokens:", len(str(self.document_query_results).split()))
+
+    def search_convo_vector_store(self):
+
+        # Retrieve and print the top three relevant chunks for each query
+        results = {}
+        for key, query in self.queries.items():
+            retrieved_chunks = self.convo_vector_store.similarity_search(query, k=self.num_of_choices)
+            results[key] = [text_chunk.page_content for text_chunk in retrieved_chunks] if retrieved_chunks else ["Not found"]
+
+        # Print the top three relevant chunks for each query
+        self.convo_query_results = {}
+        for key, contents in results.items():
+            self.convo_query_results[key] = {}
+            for i, content in enumerate(contents):
+                self.convo_query_results[key][f"Choice {i+1}"] = content
+
+        print(json.dumps(self.convo_query_results, indent=4))
+        print("Number of tokens:", len(str(self.convo_query_results).split()))
+
+    def query_from_final_results(self, similarity_search_results: dict):
+        llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+
+        for key, content in similarity_search_results.items():
+            # Truncate content to fit within token limits
+            messages = [
+                SystemMessage(content=self.systm_msg),
+                HumanMessage(content=f"query: {self.queries[key]}, content: {content}")
+            ]
+
+            response = llm(messages)
+            print(response.content)
+            print("-----")
